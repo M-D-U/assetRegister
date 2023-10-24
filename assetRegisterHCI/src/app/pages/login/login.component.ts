ea48ec8d75b8b837a3400997719d57f579147948
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-10-24 07:41:13 +0200

    login component generated

--- assetRegisterHCI/src/app/pages/login/login.component.ts
+++ assetRegisterHCI/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+// loginForm: FormGroup;
+  
+  constructor( private router:Router) {
+    /* this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required] 
+    });*/
+  }
 
+  login(){
+    console.log('we are going to the home page');
+    this.router.navigate(['home']);
+  }
 }
