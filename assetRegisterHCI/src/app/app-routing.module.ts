0067db76b18794663d15bfb2d418d9183d63f28b
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2023-10-24 07:42:32 +0200

    updated routes and components

--- assetRegisterHCI/src/app/app-routing.module.ts
+++ assetRegisterHCI/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AssetRegisterComponent } from './pages/asset-register/asset-register.component';
+import { ReportsComponent } from "./pages/reports/reports.component";
+import { UserManagementComponent } from './pages/user-management/user-management.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'assetregister', component: AssetRegisterComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path: 'usermanagement', component: UserManagementComponent},
+  {path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- assetRegisterHCI/src/app/app.module.ts
+++ assetRegisterHCI/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AssetRegisterComponent } from './pages/asset-register/asset-register.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { UserManagementComponent } from './pages/user-management/user-management.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+// import { FormsModule } from '@angular/forms';
+// import { ReactiveFormsModule } from '@angular/forms';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    AssetRegisterComponent,
+    ReportsComponent,
+    UserManagementComponent,
+    ProfileComponent,
+    // RouterModule
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    //CommonModule, // Include CommonModule here
   ],
   providers: [],
   bootstrap: [AppComponent]
