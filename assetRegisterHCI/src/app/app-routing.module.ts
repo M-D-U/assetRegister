b82d962c650e88f408a3ac05f3774bb4dbef39f1
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2024-02-16 10:59:27 +0200

    updated routing modules

--- assetRegisterHCI/src/app/app-routing.module.ts
+++ assetRegisterHCI/src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { LoginComponent } from './pages/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AssetRegisterComponent } from './pages/asset-register/asset-register.component';
-import { ReportsComponent } from "./pages/reports/reports.component";
 import { UserManagementComponent } from './pages/user-management/user-management.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
@@ -12,7 +11,6 @@ const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'home', component: HomeComponent},
   {path: 'assetregister', component: AssetRegisterComponent},
-  {path: 'reports', component: ReportsComponent},
   {path: 'usermanagement', component: UserManagementComponent},
   {path: 'profile', component: ProfileComponent }
 ];
