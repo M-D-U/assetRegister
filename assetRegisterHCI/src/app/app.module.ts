5cbaa67217b43d26e4414330a4636bcbda607a6d
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2024-02-16 11:00:20 +0200

    updated routing modules

--- assetRegisterHCI/src/app/app.module.ts
+++ assetRegisterHCI/src/app/app.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AssetRegisterComponent } from './pages/asset-register/asset-register.component';
-import { ReportsComponent } from './pages/reports/reports.component';
 import { UserManagementComponent } from './pages/user-management/user-management.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HelpusComponent } from './pages/helpus/helpus.component';
 // import { ReportsPipe } from './pages/reports.pipe';
 // import { FormsModule } from '@angular/forms';
 // import { ReactiveFormsModule } from '@angular/forms';
 // import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 // import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'helpus', component: HelpusComponent },
+  { path: 'assetregister', component: AssetRegisterComponent },
+  { path: 'usermanagement', component: UserManagementComponent },
+  { path: 'profile', component: ProfileComponent },
+];
 
 
 @NgModule({
@@ -22,15 +31,18 @@ import { ProfileComponent } from './pages/profile/profile.component';
     LoginComponent,
     HomeComponent,
     AssetRegisterComponent,
-    ReportsComponent,
     UserManagementComponent,
     ProfileComponent,
+    NavbarComponent,
+    HelpusComponent,
     // RouterModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule,
+    RouterModule.forRoot(routes) // Add this line to import the router module and configure the routes
+  
     //CommonModule, // Include CommonModule here
   ],
   providers: [],
