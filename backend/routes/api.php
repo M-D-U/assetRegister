31167fa5f31144c6d45522ae8116b67beeb78761
Mduduzi Given Skhosana <77222452+M-D-U@users.noreply.github.com>
2024-03-17 16:49:08 +0200

    updated api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IssuesController;
+use App\Models\Issue;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,24 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::resource('issues', IssueController::class);
+Route::get('/issue-counts-by-category', [IssuesController::class, 'getIssueCountsByCategory']);//bar graph
+Route::get('/issue-counts-by-resolution-status', [IssuesController::class, 'countIssuesByResolutionStatus']); //bar graph
+Route::get('/weekly-issue-stats', [IssuesController::class, 'getWeeklyIssueStats']);
+// Route::get('/weekly-issue-stats', [IssuesController::class, 'getIssueCountsByWeek']);
+Route::get('/total-issues-this-month', [IssuesController::class, 'getTotalIssuesThisMonth']);
+Route::get('/total-issues-this-week', [IssuesController::class, 'getTotalIssuesThisWeek']);
+Route::get('/issue-counts-by-service-provider', [IssuesController::class, 'getIssueCountsByServiceProvider']); //pie chart
+Route::get('/issue-counts-by-week', [IssuesController::class, 'countIssuesByWeek']);
+Route::get('/average-time-to-resolve', [IssuesController::class, 'getAverageTimeToResolve']);
+Route::get('/issues', [IssuesController::class, 'index']);
+Route::get('/issues/counts', [IssuesController::class, 'getIssueCounts']);
+// Route::get('/issues/total', [IssuesController::class, 'getTotalIssues']);
+Route::post('/issues', [IssuesController::class, 'store']);
+Route::get('/issues/{id}', [IssuesController::class, 'show']);
+Route::put('/issues/{id}', [IssuesController::class, 'update']);
+Route::delete('/issues/{id}', [IssuesController::class, 'destroy']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
